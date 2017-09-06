e73a6e27adbba2d9168089da36b454c31d9af193
unknown <zeronrats@gmail.com>
2017-09-06 21:38:29 +0300

    Second Commit2

--- web/bot.php
+++ web/bot.php
@@ -1,8 +1,12 @@
 <?php
 
 $token = "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU";
-$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=373416942&text=\"Hi! Im Bot\"";
 
+$update = json_decode(file_get_contents("https://api.telegram.org/bot" . $token . "/getUpdates"));
+
+$chatID = end($updata->result)->message->chat->id;
+
+$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=$chatID&text=\"Hi! Im Bot\"";
 file_get_contents($url);
 
 ?>
