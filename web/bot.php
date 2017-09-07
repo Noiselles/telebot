08085feae307f0b13fc42aa8abe9f18079cc14f5
unknown <zeronrats@gmail.com>
2017-09-07 15:13:05 +0300

    Second Commit2

--- web/bot.php
+++ web/bot.php
@@ -4,13 +4,13 @@ $token = "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU";
 $content = file_get_contents("php://input");
 $update = json_decode($content, TRUE);
 $message = $update[message][text];
-$chatID = $updata[message][chat][id];
+$chatID = $updata[Message][Chat][id];
 
-//$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=$chatID&text=\"TEST: $message\"";
+//$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=373416942&text=\"TEST: $message\"";
 //file_get_contents($url);
 function sendMessage($msg,$CHID)
 {
-  $sendURL = "https://api.telegram.org/bot" . "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU" . "/sendMessage?chat_id=373416942&text=$msg";
+  $sendURL = "https://api.telegram.org/bot" . "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU" . "/sendMessage?chat_id=$CHID&text=$msg";
   file_get_contents($sendURL);
 }
 if(strpos($message,"/start") !== false)
