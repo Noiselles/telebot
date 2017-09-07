b2419aa5872eb6862ded0bf2eec36a518a8e22e1
unknown <zeronrats@gmail.com>
2017-09-07 19:36:44 +0300

    Second Commit2

--- web/bot.php
+++ web/bot.php
@@ -12,6 +12,41 @@ function sendMessage($msg,$CHID)
   $sendURL = "https://api.telegram.org/bot" . "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU" . "/sendMessage?chat_id=$CHID&text=" . urlencode($msg);
   file_get_contents($sendURL);
 }
+function ShowLessons($day,$CHID)
+{
+  $data = json_decode(file_get_contents("$day.txt"));
+  sendMessage(
+    "Lesson: " . $data[0] . "\n" .
+    "Lesson: " . $data[1] . "\n" .
+    "Lesson: " . $data[2] . "\n" .
+    "Lesson: " . $data[3] . "\n" .
+    "Lesson: " . $data[4] . "\n" .
+    "Lesson: " . $data[5] . "\n" .
+    "Lesson: " . $data[6] . "\n" .
+    "Lesson: " . $data[7] . "\n" ,
+  $chatID);
+}
+function AddHomework($day,$homework,$CHID)
+{
+  try
+  {
+    if(!file_exists("$day.txt"))
+    {
+      $file = fopen("$day.txt","w+");
+      fwrite($file,json_encode(array("","","","","","","","")));
+      fclose($file);
+    }
+    $fcont = json_decode(file_get_contents("$day.txt"));
+    $text = substr(substr($homework, strpos($homework, '-add') + 5, strlen($homework)),0,-1);
+    $fcont[substr($homework,-1)] = $text;
+    $file = fopen("$day.txt","w+");
+    fwrite($file,json_encode($fcont));
+    fclose($file);
+    sendMessage("Success!",$CHID);
+  } catch (Exception $e) {
+    sendMessage("Err!",$CHID);
+  }
+}
 if(strpos($message,"/start") !== false)
 {
   sendMessage("Hello " . $update[message][from][first_name] . "! \nIm Shkolo_Bot v0.0.4 \n \nEnter /help to view the commands.",$chatID);
@@ -20,115 +55,96 @@ if(strpos($message,"/help") !== false)
 {
   sendMessage("Shkolo_Bot | Commands: \n /lessons -- view homework / timetable",$chatID);
 }
-if(strcasecmp($message,"/lessons") == 0)
+if(strcasecmp($message,"/lessons -help") == 0)
 {
   sendMessage("Shkolo_Bot | /lessons commands: \n  -mo Monday\n  -tu Tuesday\n  -we Wednesday\n  -th Thursday\n  -fr Friday",$chatID);
 }
+if(strcasecmp($message,"/lessons") == 0)
+{
+  $day = date("l");
+  if(strcasecmp($day,"Monday") == 0)
+  {
+    ShowLessons("Monday",$chatID);
+  }
+  if(strcasecmp($day,"Tuesday") == 0)
+  {
+    ShowLessons("Tuesday",$chatID);
+  }
+  if(strcasecmp($day,"Wednesday") == 0)
+  {
+    ShowLessons("Wednesday",$chatID);
+  }
+  if(strcasecmp($day,"Thursday") == 0)
+  {
+    ShowLessons("Thursday",$chatID);
+  }
+  if(strcasecmp($day,"Friday") == 0)
+  {
+    ShowLessons("Friday",$chatID);
+  }
+}
+if(strcasecmp($message,"/lessons -add") == 0)
+{
+  $day = date("l");
+  if(strpos($day,"Monday") !== false)
+  {
+    AddHomework("Monday",$message,$chatID);
+  }
+  if(strpos($day,"Tuesday") !== false)
+  {
+    AddHomework("Tuesday",$message,$chatID);
+  }
+  if(strpos($day,"Wednesday") !== false)
+  {
+    AddHomework("Wednesday",$message,$chatID);
+  }
+  if(strpos($day,"Thursday") !== false)
+  {
+    AddHomework("Thursday",$message,$chatID);
+  }
+  if(strpos($day,"Friday") !== false)
+  {
+    AddHomework("Friday",$message,$chatID);
+  }
+}
 if(strcasecmp($message,"/lessons -mo") == 0)
 {
-  $data = json_decode(file_get_contents("Monday.txt"));
-  sendMessage(
-    "Lesson: " . $data[0] . "\n" .
-    "Lesson: " . $data[1] . "\n" .
-    "Lesson: " . $data[2] . "\n" .
-    "Lesson: " . $data[3] . "\n" .
-    "Lesson: " . $data[4] . "\n" .
-    "Lesson: " . $data[5] . "\n" .
-    "Lesson: " . $data[6] . "\n" .
-    "Lesson: " . $data[7] . "\n" ,
-  $chatID);
+  ShowLessons("Monday",$chatID);
 }
 if(strcasecmp($message,"/lessons -tu") == 0)
 {
-  $data = json_decode(file_get_contents("Tuesday.txt"));
-  sendMessage(
-    "Lesson: " . $data[0] . "\n" .
-    "Lesson: " . $data[1] . "\n" .
-    "Lesson: " . $data[2] . "\n" .
-    "Lesson: " . $data[3] . "\n" .
-    "Lesson: " . $data[4] . "\n" .
-    "Lesson: " . $data[5] . "\n" .
-    "Lesson: " . $data[6] . "\n" .
-    "Lesson: " . $data[7] . "\n" ,
-  $chatID);
+  ShowLessons("Tuesday",$chatID);
 }
 if(strcasecmp($message,"/lessons -we") == 0)
 {
-  $data = json_decode(file_get_contents("Wednesday.txt"));
-  sendMessage(
-    "Lesson: " . $data[0] . "\n" .
-    "Lesson: " . $data[1] . "\n" .
-    "Lesson: " . $data[2] . "\n" .
-    "Lesson: " . $data[3] . "\n" .
-    "Lesson: " . $data[4] . "\n" .
-    "Lesson: " . $data[5] . "\n" .
-    "Lesson: " . $data[6] . "\n" .
-    "Lesson: " . $data[7] . "\n" ,
-  $chatID);
+  ShowLessons("Wednesday",$chatID);
 }
 if(strcasecmp($message,"/lessons -th") == 0)
 {
-  $data = json_decode(file_get_contents("Thursday.txt"));
-  sendMessage(
-    "Lesson: " . $data[0] . "\n" .
-    "Lesson: " . $data[1] . "\n" .
-    "Lesson: " . $data[2] . "\n" .
-    "Lesson: " . $data[3] . "\n" .
-    "Lesson: " . $data[4] . "\n" .
-    "Lesson: " . $data[5] . "\n" .
-    "Lesson: " . $data[6] . "\n" .
-    "Lesson: " . $data[7] . "\n" ,
-  $chatID);
+  ShowLessons("Thursday",$chatID);
 }
 if(strcasecmp($message,"/lessons -fr") == 0)
 {
-  $data = json_decode(file_get_contents("Friday.txt"));
-  sendMessage(
-    "Lesson: " . $data[0] . "\n" .
-    "Lesson: " . $data[1] . "\n" .
-    "Lesson: " . $data[2] . "\n" .
-    "Lesson: " . $data[3] . "\n" .
-    "Lesson: " . $data[4] . "\n" .
-    "Lesson: " . $data[5] . "\n" .
-    "Lesson: " . $data[6] . "\n" .
-    "Lesson: " . $data[7] . "\n" ,
-  $chatID);
+  ShowLessons("Friday",$chatID);
 }
 if(strpos($message,"/lessons -mo -add") !== false)
 {
-  try
-  {
-    if(!file_exists("Monday.txt"))
-    {
-      $file = fopen("Monday.txt","w+");
-      fwrite($file,json_encode(array("","","","","","","","")));
-      fclose($file);
-    }
-    $fcont = json_decode(file_get_contents("Monday.txt"));
-    $text = substr(substr($message, strpos($message, '-add') + 5, strlen($message)),0,-1);
-    $fcont[substr($message,-1)] = $text;
-    $file = fopen("Monday.txt","w+");
-    fwrite($file,json_encode($fcont));
-    fclose($file);
-    sendMessage("Success! Data: \n" . $text . "\n" . $fcont[substr($message,-1)],$chatID);
-  } catch (Exception $e) {
-    sendMessage("Err!",$chatID);
-  }
+  AddHomework("Monday",$message,$chatID);
 }
 if(strpos($message,"/lessons -tu -add") !== false)
 {
-  sendMessage("Success!",$chatID);
+  AddHomework("Tuesday",$message,$chatID);
 }
 if(strpos($message,"/lessons -we -add") !== false)
 {
-  sendMessage("Success!",$chatID);
+  AddHomework("Wednesday",$message,$chatID);
 }
 if(strpos($message,"/lessons -th -add") !== false)
 {
-  sendMessage("Success!",$chatID);
+  AddHomework("Thursday",$message,$chatID);
 }
 if(strpos($message,"/lessons -fr -add") !== false)
 {
-  sendMessage("Success!",$chatID);
+  AddHomework("Friday",$message,$chatID);
 }
 ?>
