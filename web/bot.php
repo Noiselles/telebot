32a426f1395ca630f867b70c523a081f76874722
unknown <zeronrats@gmail.com>
2017-09-07 15:55:23 +0300

    Second Commit2

--- web/bot.php
+++ web/bot.php
@@ -9,12 +9,12 @@ $chatID = $update[message][chat][id];
 //file_get_contents($url);
 function sendMessage($msg,$CHID)
 {
-  $sendURL = "https://api.telegram.org/bot" . "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU" . "/sendMessage?chat_id=$CHID&text=$msg";
+  $sendURL = "https://api.telegram.org/bot" . "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU" . "/sendMessage?chat_id=$CHID&text=$msg&parse_mode=html";
   file_get_contents($sendURL);
 }
 if(strpos($message,"/start") !== false)
 {
-  sendMessage("Hello! \n :D",$chatID);
+  sendMessage("Hello! <br/> :D",$chatID);
 }
 
 ?>
