700d15ebc1b956baf4becae1ee37cb81b71e50c1
unknown <zeronrats@gmail.com>
2017-09-07 14:56:25 +0300

    Second Commit2

--- web/bot.php
+++ web/bot.php
@@ -9,7 +9,7 @@ $chatID = $updata[message][chat][id];
 //$url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=$chatID&text=\"TEST: $message\"";
 //file_get_contents($url);
 
-if(strpos($message,"/help"))
+if(strpos($message,"/help") !== false)
 {
   $token = "399710240:AAG5WJkoNlgPYL2RPOfZ4BeEmKlvuTimfuU";
   $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=373416942&text=\"TEST: $message\"";
